33d22a219d5693565f7bee27d11e9a4fb713496f
archupatil-90 <73097336+archupatil-90@users.noreply.github.com>
2021-03-06 17:16:06 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,20 @@
+let items = document.querySelectorAll('button');
+let pictures = document.querySelectorAll('.portfolio-wrap img');
+
+items.forEach(button => {
+    button.onclick = function() {
+        items.forEach(button => {
+            button.className = "";
+        })
+        button.className = "Active";
+
+        let value = button.textContent;
+        pictures.forEach(img => {
+            img.style.display = 'none';
+            if (img.getAttribute('data-filter') == value.toLowerCase() || value == "All") {
+                img.style.display = 'block';
+            }
+        })
+    }
+});
+    
