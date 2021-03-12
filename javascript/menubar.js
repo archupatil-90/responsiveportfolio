049457057de4f3ccf14173c017f3883963ea460a
archupatil-90 <73097336+archupatil-90@users.noreply.github.com>
2021-03-12 15:24:46 +0530

    Add files via upload

---
+++ javascript/menubar.js
@@ -0,0 +1,7 @@
+
+   const toglebtn = document.getElementsByClassName("toggle-button")[0]
+const menubar = document.getElementsByClassName("navbar-links")[0]
+toglebtn.addEventListener('click' , () =>{
+menubar.classList.toggle("active")
+
+})
